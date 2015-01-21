2293698233192df6ed70193a60d98dccee5668ff
Yura Revutskiy <dr.bleedjent@gmail.com>
2015-01-21 15:05:44 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ab_base_templates',
+    version='0.1',
+    packages=['base_template'],
+    url='',
+    license='',
+    author='Dr.bleedjent',
+    author_email='dr.bleedjent',
+    description='Avtobazar Base templates for child projects'
+)
